0f4d64a731eac9470fe929c842438d01769dd09c
cterrizzi <cterrizzi@cterrizzis-Mac.local>
2017-12-17 18:58:50 -0800

    First commit

--- Jedi or Sith/ViewController.swift
+++ Jedi or Sith/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +21,43 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet private var buttonRayJedi: UIButton!
+    @IBOutlet private var buttonRaySith: UIButton!
+    @IBOutlet private var buttonKyloJedi: UIButton!
+    @IBOutlet private var buttonKyloSith: UIButton!
+    @IBOutlet private var labelOutput: UILabel!
+    @IBOutlet private var blueLightSaber: UIImageView!
+    @IBOutlet private var redLightSaber: UIImageView!
+    
+    @IBAction func rJediPressed(_ sender: UIButton) {
+        hideAll()
+        labelOutput.text = "Ray chose the light side"
+        labelOutput.isHidden = false
+        blueLightSaber.isHidden = false
+    }
+    @IBAction func rSithPressed(_ sender: UIButton) {
+        hideAll()
+        labelOutput.text = "Ray chose the dark side"
+        labelOutput.isHidden = false
+        redLightSaber.isHidden = false
+    }
+    @IBAction func kJediPressed(_ sender: UIButton) {
+        hideAll()
+        labelOutput.text = "Kylo chose the light side"
+        labelOutput.isHidden = false
+        blueLightSaber.isHidden = false
+    }
+    @IBAction func kSithPressed(_ sender: UIButton) {
+        hideAll()
+        labelOutput.text = "Kylo chose the dark side"
+        labelOutput.isHidden = false
+        redLightSaber.isHidden = false
+    }
+    func hideAll(){
+        labelOutput.isHidden = true
+        blueLightSaber.isHidden = true
+        redLightSaber.isHidden = true
+    }
 }
+// Was unable to get the simulator working, but it seems like it should work fine and it builds successfully
 
